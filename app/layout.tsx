961955c3ddc5a03db96dc1a0b4de12dee34c00ba
shinjith-dev <shinjithpallikkara@gmail.com>
2024-08-12 21:06:39 +0530

    chore: next-supabase template cleanup
    Signed-off-by: shinjith-dev <shinjithpallikkara@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Thotbit",
+  description: "Think in bites, share in notes. Thotbit is a modern note-sharing platform for developers. Thotbit allows people to share their knowledge, insights, and experiences through brief notes.",
 };
 
 export default function RootLayout({
